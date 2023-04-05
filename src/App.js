ad9ec73620df280535d918912ceeeb96b1031846
REJOINT <brecab120@gmail.com>
2023-04-04 22:13:46 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Self from './Components/GitHubCard';
+//import renderer from 'react-test-render';
 
-function App() {
+function App(Self) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/Components/GitHubCard.js
@@ -0,0 +1,19 @@
+import Card from 'react-bootstrap/Card';
+
+function Self() {
+  return (
+    <>
+      <Card>
+        <Card.Title>Rejointing</Card.Title>
+        <Card.Img variant="top" src="https://cdn.shopify.com/s/files/1/0554/5295/8804/products/55E108B2-6895-46E2-BEEC-1EA249009475.jpg?v=1664997451&width=1100" />
+        <Card.Body>
+          <Card.Text>
+            Hello! I go by Rejointing. I love to code, paint, and eat sushi!
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </>
+  );
+}
+
+export default Self;
